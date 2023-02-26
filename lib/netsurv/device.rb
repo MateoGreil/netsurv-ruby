837245f61f989de3d70c87938aafde52c5c756a6
Mateo Greil <mat@greil.fr>
2023-02-26 18:18:03 +0100

    add frozen literal string

--- lib/netsurv/device.rb
+++ lib/netsurv/device.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module NetSurv
   # Handle all camera functions
   class Device
--- lib/netsurv/devices/camera.rb
+++ lib/netsurv/devices/camera.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 module NetSurv
   # This is the camera class : it handle all camera functions
   class Camera < Device; end
